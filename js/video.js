528def9a911547e36946c40a86ed19c9daa388ec
Munthasir Rahman <munthasirrahman@gmail.com>
2026-05-13 02:00:24 +0600

    feat: add responsive styles and link to responsive.css, remove unused utility functions from video.js

--- js/video.js
+++ js/video.js
@@ -1,19 +1,4 @@
 /* ===================================
-   UTILITY FUNCTIONS
-   =================================== */
-function debounce(func, wait) {
-    let timeout;
-    return function executedFunction(...args) {
-        const later = () => {
-            clearTimeout(timeout);
-            func(...args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
-}
-
-/* ===================================
    VIDEO CONFIG
    =================================== */
 const VIDEO_CONFIG = {
