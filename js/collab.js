99d46a99547516cac5fc2aeb02777716c6ef3930
Munthasir Rahman <munthasirrahman@gmail.com>
2026-05-31 18:55:30 +0600

    refactor: remove 3D tilt, glow effects, and hover animations from collaboration cards

--- js/collab.js
+++ js/collab.js
@@ -20,36 +20,4 @@ function initCollabAnimations() {
 
   cards.forEach((card) => cardObserver.observe(card));
 
-  cards.forEach((card) => {
-    card.addEventListener("mousemove", handleCardMouseMove);
-    card.addEventListener("mouseleave", handleCardMouseLeave);
-  });
-}
 
-function handleCardMouseMove(e) {
-  const card = e.currentTarget;
-  const rect = card.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-  const centerX = rect.width / 2;
-  const centerY = rect.height / 2;
-  const rotateX = (y - centerY) / 20;
-  const rotateY = (centerX - x) / 20;
-
-  card.style.setProperty("--rotate-x", rotateX + "deg");
-  card.style.setProperty("--rotate-y", rotateY + "deg");
-  card.style.transform = [
-    "perspective(1000px)",
-    `rotateX(${rotateX}deg)`,
-    `rotateY(${rotateY}deg)`,
-    "translateY(-12px)",
-    "scale(1.02)",
-  ].join(" ");
-}
-
-function handleCardMouseLeave(e) {
-  const card = e.currentTarget;
-  card.style.removeProperty("--rotate-x");
-  card.style.removeProperty("--rotate-y");
-  card.style.transform = "";
-}
